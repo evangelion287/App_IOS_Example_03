38f142dec825ccdc30e99a6bd50c2c69cde08f19
ALB <albertomtz@gmail.com>
2022-02-26 08:03:13 -0600

    Initial Commit

---
+++ SideMenuSwiftTutorial/SideMenuSwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftTutorialApp.swift
+//  SideMenuSwiftTutorial
+//
+//  Created by Mac on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

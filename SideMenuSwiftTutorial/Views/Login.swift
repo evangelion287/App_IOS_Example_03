ea6a7de7ff3c98abc1ba3cd0465ac6f79ba38f45
Alberto <albertomtzu@gmail.com>
2022-05-30 22:35:52 -0500

    Se genera pantalla de Login

---
+++ SideMenuSwiftTutorial/Views/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  SideMenuSwiftTutorial
+//
+//  Created by ALBERTO on 30/05/22.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
